68bd6f2b8180e7f0705dc08d1d54dc546e9a3eff
WhoKnowss2 <76677738+WhoKnowss2@users.noreply.github.com>
2026-05-01 09:19:34 -0700

    number guessing game

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Setting playAgain
+        string playAgain = "yes";
+
+        while (playAgain == "yes")
+        {
+            // Getting random number
+            Random random = new Random();
+            int magicNumber = random.Next(1, 101);
+
+            // Counting guesses
+            int guess = -1;
+            int guessCount = 0;
+
+            // Guessing loop
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"You made {guessCount} guesses.");
+                }
+            }
+            // Check if user wants to play again
+            Console.Write("Play again? (yes/no) ");
+            playAgain = Console.ReadLine().ToLower();
+        }
     }
 }
